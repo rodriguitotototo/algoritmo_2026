a11b9473ba6c304c53ae7e13c6387ac7fe0bf3ca
rodrigo <penayorodrigo8@gmail.com>
2026-04-28 13:33:18 -0300

    trabajo 5 terminado

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,21 +1,17 @@
 
  #  Desarrollar una función que permita convertir un número romano en un
  #  número decimal.
-def romano_decimal(r):
+
+def romano_decimal(r, i=0):
     val = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    
-    res = 0
-    i = 0
 
-    while i < len(r):
-        if i < len(r)-1 and val[r[i]] < val[r[i+1]]:
-            res = res + (val[r[i+1]] - val[r[i]])
-            i += 2
-        else:
-            res = res + val[r[i]]
-            i += 1
+    if i >= len(r):
+        return 0
 
-    return res
+    if i < len(r)-1 and val[r[i]] < val[r[i+1]]:
+        return (val[r[i+1]] - val[r[i]]) + romano_decimal(r, i+2)
+    else:
+        return val[r[i]] + romano_decimal(r, i+1)
 
 
 num = input("romano: ").upper()
