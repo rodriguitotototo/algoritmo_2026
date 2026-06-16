855bbecec49098c950190aa9db3bbf976c0de64b
rodrigo <penayorodrigo8@gmail.com>
2026-06-15 21:12:48 -0300

    se subieron todos los ejercicios de cola

--- COLAS/ejercicio10,COLA.py
+++ COLAS/ejercicio10,COLA.py
@@ -1,88 +1,145 @@
 
+class Nodo:
+    def __init__(self, info):
+        self.info = info
+        self.sig = None
 
-cola_notificaciones = [
-    ["08:15", "Facebook",  "Juan comentó tu foto"],
-    ["09:00", "Twitter",   "Nuevo tweet sobre Python y datos"],
-    ["10:20", "Facebook",  "Tienes 3 solicitudes de amistad"],
-    ["12:00", "Twitter",   "Curso gratuito de Python disponible"],
-    ["13:45", "Twitter",   "Python supera a Java en popularidad"],
-    ["14:15", "Instagram", "A Pedro le gustó tu historia"],
-    ["15:00", "Facebook",  "Cumpleaños de Ana mañana"],
-    ["16:00", "Twitter",   "Workshop de programación"]
-]
-for n in cola_notificaciones:
-    print(n)
-print()
+class Cola:
+    def __init__(self):
+        self.frente = None
+        self.final = None
 
-# A) Eliminar todas las notificaciones de Facebook
+    def arribo(self, dato):
+        nuevo = Nodo(dato)
+        if self.frente is None:
+            self.frente = nuevo
+            self.final = nuevo
+        else:
+            self.final.sig = nuevo
+            self.final = nuevo
 
+    def atencion(self):
+        if self.frente is None:
+            return None
+        dato = self.frente.info
+        self.frente = self.frente.sig
+        if self.frente is None:
+            self.final = None
+        return dato
+
+    def esta_vacia(self):
+        return self.frente is None
+
+class Pila:
+    def __init__(self):
+        self.cima = None
+
+    def apilar(self, dato):
+        nuevo = Nodo(dato)
+        nuevo.sig = self.cima
+        self.cima = nuevo
+
+    def desapilar(self):
+        if self.cima is None:
+            return None
+        dato = self.cima.info
+        self.cima = self.cima.sig
+        return dato
+
+    def esta_vacia(self):
+        return self.cima is None
+
+
+# Funciones del Ejercicio
+
+def mostrar_cola(cola):
+    cola_aux = Cola()
+    while not cola.esta_vacia():
+        notif = cola.atencion()
+        print(f"Hora: {notif[0]} | App: {notif[1]} | Mensaje: {notif[2]}")
+        cola_aux.arribo(notif)
+    
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
+
+
+# a)
 def eliminar_facebook(cola):
-    cola_auxiliar = []
-
-    while len(cola) > 0:
-        notificacion = cola.pop(0)
-        app = notificacion[1]
-        if app != "Facebook":
-            cola_auxiliar.append(notificacion)
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
+    cola_aux = Cola()
+    while not cola.esta_vacia():
+        notif = cola.atencion()
+        if notif[1] != "Facebook":
+            cola_aux.arribo(notif)
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
 
 
-# B) Mostrar notificaciones de Twitter con 'Python' (sin perder datos)
-
+# b) 
 def mostrar_twitter_python(cola):
-    cola_auxiliar = []
-
-    while len(cola) > 0:
-        notificacion = cola.pop(0)
-        app = notificacion[1]
-        mensaje = notificacion[2]
-
-        if app == "Twitter" and "Python" in mensaje:
-            print(f"Hora: {notificacion[0]} | Mensaje: {mensaje}")
-
-        cola_auxiliar.append(notificacion)
-
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-
-
-# C) Pila temporal para notificaciones entre las 11:43 y las 15:57
-
-def notificaciones_en_rango(cola):
-    pila_temporal = []
-    cola_auxiliar = []
-    
-    while len(cola) > 0:
-        notificacion = cola.pop(0)
-        hora = notificacion[0]
+    cola_aux = Cola()
+    while not cola.esta_vacia():
+        notif = cola.atencion()
         
-        if "11:43" <= hora <= "15:57":
-            pila_temporal.append(notificacion)
+        
+        if notif[1] == "Twitter" and "Python" in notif[2]:
+            print(f"-> [{notif[0]}] {notif[2]}")
             
-        cola_auxiliar.append(notificacion)
+        cola_aux.arribo(notif) 
         
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
-    print(f"Cantidad de notificaciones en la pila: {len(pila_temporal)}")
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
+
+
+# c) 
+def notificaciones_en_rango(cola):
+    pila_temporal = Pila()
+    cola_aux = Cola()
+    contador = 0
     
-    print("Mostrando contenido de la pila (desde la última que entró):")
-    while len(pila_temporal) > 0:
-        notif_pila = pila_temporal.pop()
-        print(f"[{notif_pila[0]}] {notif_pila[1]}: {notif_pila[2]}")
+    while not cola.esta_vacia():
+        notif = cola.atencion()
+        if "11:43" <= notif[0] <= "15:57":
+            pila_temporal.apilar(notif)
+            contador += 1
+        cola_aux.arribo(notif)
+        
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
+        
+    print(f"Cantidad de notificaciones en el rango: {contador}")
+    print("Mostrando contenido de la Pila (Orden LIFO):")
+    while not pila_temporal.esta_vacia():
+        n_pila = pila_temporal.desapilar()
+        print(f"   [{n_pila[0]}] {n_pila[1]}: {n_pila[2]}")
 
 
+# Carga de datos 
 
-print("--- PUNTO B: Twitter con 'Python' ---")
+cola_notificaciones = Cola()
+datos = [
+    ("08:15", "Facebook",  "Juan comentó tu foto"),
+    ("09:00", "Twitter",   "Nuevo tweet sobre Python y datos"),
+    ("10:20", "Facebook",  "Tienes 3 solicitudes de amistad"),
+    ("12:00", "Twitter",   "Curso gratuito de Python disponible"),
+    ("13:45", "Twitter",   "Python es genial"),
+    ("14:15", "Instagram", "A Pedro le gustó tu historia"),
+    ("15:00", "Facebook",  "Cumpleaños de Ana mañana"),
+    ("16:00", "Twitter",   "Workshop de programación")
+]
+
+for d in datos:
+    cola_notificaciones.arribo(d)
+
+print("--- COLA ORIGINAL ---")
+mostrar_cola(cola_notificaciones)
+
+print("\n--- PUNTO B (Twitter con Python) ---")
 mostrar_twitter_python(cola_notificaciones)
 
-print("\n--- PUNTO C: Pila con rango de horas ---")
+print("\n--- PUNTO C (Pila en rango horario) ---")
 notificaciones_en_rango(cola_notificaciones)
 
-print("\n--- PUNTO A: Eliminando Facebook ---")
+print("\n--- PUNTO A (Eliminar Facebook) ---")
 eliminar_facebook(cola_notificaciones)
-print()
-print("Cola final sin las de Facebook:")
-for n in cola_notificaciones:
-    print(n)
+print("Cola final resultante:")
+mostrar_cola(cola_notificaciones)
--- COLAS/ejercicio22,COLA.py
+++ COLAS/ejercicio22,COLA.py
@@ -1,142 +1,184 @@
 
+class Nodo:
+    def __init__(self, info):
+        self.info = info
+        self.sig = None
 
+class Cola:
+    def __init__(self):
+        self.frente = None
+        self.final = None
+        self.tamanio = 0
 
-cola_personajes = [
-    ["Tony Stark",        "Iron Man",             "M"],
-    ["Steve Rogers",      "Capitan America",      "M"],
-    ["Natasha Romanoff",  "Black Widow",          "F"],
-    ["Bruce Banner",      "Hulk",                 "M"],
-    ["Thor Odinson",      "Thor",                 "M"],
-    ["Clint Barton",      "Hawkeye",              "M"],
-    ["Carol Danvers",     "Capitana Marvel",      "F"],
-    ["Scott Lang",        "Ant-Man",              "M"],
-    ["Peter Parker",      "Spider-Man",           "M"],
-    ["Stephen Strange",   "Doctor Strange",       "M"],
-    ["Wanda Maximoff",    "Scarlet Witch",        "F"],
-    ["Sam Wilson",        "Falcon",               "M"],
-    ["T'Challa",          "Black Panther",        "M"],
-    ["Shuri",             "Shuri",                "F"],
-    ["Hope Van Dyne",     "The Wasp",             "F"]
-]
+    def arribo(self, dato):
+        nuevo = Nodo(dato)
+        if self.final is not None:
+            self.final.sig = nuevo
+        self.final = nuevo
+        if self.frente is None:
+            self.frente = nuevo
+        self.tamanio += 1
+
+    def atencion(self):
+        if self.frente is None:
+            return None
+        dato = self.frente.info
+        self.frente = self.frente.sig
+        if self.frente is None:
+            self.final = None
+        self.tamanio -= 1
+        return dato
+
+    def esta_vacia(self):
+        return self.frente is None
+
+    def en_frente(self):
+        if self.frente is not None:
+            return self.frente.info
+        return None
+
+def mostrar_cola(cola):
+    cola_aux = Cola()
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        print(f"Personaje: {personaje} | Superheroe: {superheroe} | Genero: {genero}")
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
+
+def cargar_personajes():
+    cola = Cola()
+    personajes = [
+        ("Tony Stark",        "Iron Man",             "M"),
+        ("Steve Rogers",      "Capitan America",      "M"),
+        ("Natasha Romanoff",  "Black Widow",          "F"),
+        ("Bruce Banner",      "Hulk",                 "M"),
+        ("Thor Odinson",      "Thor",                 "M"),
+        ("Clint Barton",      "Hawkeye",              "M"),
+        ("Carol Danvers",     "Capitana Marvel",      "F"),
+        ("Scott Lang",        "Ant-Man",              "M"),
+        ("Peter Parker",      "Spider-Man",           "M"),
+        ("Stephen Strange",   "Doctor Strange",       "M"),
+        ("Wanda Maximoff",    "Scarlet Witch",        "F"),
+        ("Sam Wilson",        "Falcon",               "M"),
+        ("T'Challa",          "Black Panther",        "M"),
+        ("Shuri",             "Shuri",                "F"),
+        ("Hope Van Dyne",     "The Wasp",             "F"),
+    ]
+    for p in personajes:
+        cola.arribo(p)
+    return cola
 
 # Punto A
 def buscar_personaje_por_superheroe(cola, nombre_superheroe):
-    cola_auxiliar = []
+    cola_aux = Cola()
     resultado = None
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[1].lower() == nombre_superheroe.lower():
-            resultado = item[0]
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        if superheroe.lower() == nombre_superheroe.lower():
+            resultado = personaje
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
     return resultado
 
 # Punto B
 def mostrar_superheroes_femeninos(cola):
-    cola_auxiliar = []
+    cola_aux = Cola()
     contador = 0
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[2] == "F":
+    print("\nSuperheroes femeninos:")
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        if genero == "F":
             contador += 1
-            print(f"{contador}. {item[1]}")
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
+            print(f"{contador}. {superheroe}")
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
     return contador
 
 # Punto C
 def mostrar_personajes_masculinos(cola):
-    cola_auxiliar = []
+    cola_aux = Cola()
     contador = 0
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[2] == "M":
+    print("\nPersonajes masculinos:")
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        if genero == "M":
             contador += 1
-            print(f"{contador}. {item[0]}")
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
+            print(f"{contador}. {personaje}")
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
     return contador
 
 # Punto D
 def buscar_superheroe_por_personaje(cola, nombre_personaje):
-    cola_auxiliar = []
+    cola_aux = Cola()
     resultado = None
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[0].lower() == nombre_personaje.lower():
-            resultado = item[1]
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        if personaje.lower() == nombre_personaje.lower():
+            resultado = superheroe
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
     return resultado
 
 # Punto E
 def mostrar_nombres_con_letra(cola, letra):
-    cola_auxiliar = []
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[0][0].upper() == letra.upper() or item[1][0].upper() == letra.upper():
-            print(f"Nombre: {item[0]} | Superheroe: {item[1]} | Genero: {item[2]}")
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
+    cola_aux = Cola()
+    print(f"\nPersonajes/Superheroes que empiezan con {letra}:")
+    while not cola.esta_vacia():
+        personaje, superheroe, genero = cola.atencion()
+        if personaje[0].upper() == letra.upper() or superheroe[0].upper() == letra.upper():
+            print(f"Nombre: {personaje} | Superheroe: {superheroe} | Genero: {genero}")
+        cola_aux.arribo((personaje, superheroe, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
 
 # Punto F
 def buscar_personaje(cola, nombre_buscado):
-    cola_auxiliar = []
+    cola_aux = Cola()
     encontrado = False
     superheroe = None
-    
-    while len(cola) > 0:
-        item = cola.pop(0)
-        if item[0].lower() == nombre_buscado.lower():
+    while not cola.esta_vacia():
+        personaje, superheroe_actual, genero = cola.atencion()
+        if personaje.lower() == nombre_buscado.lower():
             encontrado = True
-            superheroe = item[1]
-        cola_auxiliar.append(item)
-        
-    while len(cola_auxiliar) > 0:
-        cola.append(cola_auxiliar.pop(0))
-        
+            superheroe = superheroe_actual
+        cola_aux.arribo((personaje, superheroe_actual, genero))
+    while not cola_aux.esta_vacia():
+        cola.arribo(cola_aux.atencion())
     return encontrado, superheroe
 
-# Pruebas en consola
-print("--- PUNTO A: Personaje de Capitana Marvel ---")
-personaje_cm = buscar_personaje_por_superheroe(cola_personajes, "Capitana Marvel")
-print(f"El personaje es: {personaje_cm}")
+# Main
+if __name__ == "__main__":
+    cola_personajes = cargar_personajes()
+    
+    print("--- COLA COMPLETA ---")
+    mostrar_cola(cola_personajes)
 
-print("\n--- PUNTO B: Superheroes femeninos ---")
-mostrar_superheroes_femeninos(cola_personajes)
+    print("\n--- PUNTO A ---")
+    personaje_cm = buscar_personaje_por_superheroe(cola_personajes, "Capitana Marvel")
+    print(f"El personaje de Capitana Marvel es: {personaje_cm}")
 
-print("\n--- PUNTO C: Personajes masculinos ---")
-mostrar_personajes_masculinos(cola_personajes)
+    print("\n--- PUNTO B ---")
+    mostrar_superheroes_femeninos(cola_personajes)
 
-print("\n--- PUNTO D: Superheroe de Scott Lang ---")
-superheroe_sl = buscar_superheroe_por_personaje(cola_personajes, "Scott Lang")
-print(f"El superheroe es: {superheroe_sl}")
+    print("\n--- PUNTO C ---")
+    mostrar_personajes_masculinos(cola_personajes)
 
-print("\n--- PUNTO E: Nombres que empiezan con S ---")
-mostrar_nombres_con_letra(cola_personajes, "S")
+    print("\n--- PUNTO D ---")
+    superheroe_sl = buscar_superheroe_por_personaje(cola_personajes, "Scott Lang")
+    print(f"El superheroe de Scott Lang es: {superheroe_sl}")
 
-print("\n--- PUNTO F: Buscar a Carol Danvers ---")
-encontrada, superheroe_cd = buscar_personaje(cola_personajes, "Carol Danvers")
-if encontrada:
-    print(f"Carol Danvers SI esta en la cola. Su superheroe es: {superheroe_cd}")
-else:
-    print("Carol Danvers NO esta en la cola.")
+    print("\n--- PUNTO E ---")
+    mostrar_nombres_con_letra(cola_personajes, "S")
+
+    print("\n--- PUNTO F ---")
+    encontrada, superheroe_cd = buscar_personaje(cola_personajes, "Carol Danvers")
+    if encontrada:
+        print(f"Carol Danvers SI esta en la cola. Superheroe: {superheroe_cd}")
+    else:
+        print("Carol Danvers NO esta en la cola.")
